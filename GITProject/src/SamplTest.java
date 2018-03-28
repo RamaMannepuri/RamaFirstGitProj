e78c0208d00b2013ec9fb582aa62fb86fe503284
ramalakshmi.eerpina <ramalakshmi.eerpina@NTPC01926.FPS.NIHILENT.com>
2018-03-28 18:07:54 +0530

    first commit

---
+++ GITProject/src/SamplTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ramalakshmi.eerpina
+ *
+ */
+public class SamplTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
